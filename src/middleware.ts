f75904161f9cdfc0f60142a36eaaa9304e29044b
unknown <charlie@balancepoint.com>
2023-06-29 14:29:01 -0400

    middleware fix and style skeleton

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-export default authMiddleware();
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
